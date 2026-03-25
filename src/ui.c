dc1dbc6ff6006bc382306ec26e0a8ab1d2d7daa9
Swayamrishi Satyaprakash Patra <swayamrishipatra@gmail.com>
2026-03-25 21:56:48 +0530

    Update ui.c

--- src/ui.c
+++ src/ui.c
@@ -2,15 +2,15 @@
 #include "../include/ui.h"
 
 void displayMenu() {
-    printf("\n==================================\n");
-    printf("1. Add Product\n");
-    printf("2. Update Stock (Purchase/Sale)\n");
-    printf("3. Delete Product\n");
-    printf("4. Search by ProductID\n");
+    printf("\nInventory Management System:\n");
+    printf("1. Add\n");
+    printf("2. Update\n");
+    printf("3. Delete\n");
+    printf("4. Search\n");
     printf("5. Low-stock Report\n");
-    printf("6. Show Total Inventory Value\n");
+    printf("6. Total Inventory Value\n");
     printf("7. Show All Products\n");
     printf("8. Save and Exit\n");
-    printf("==================================\n");
+    printf("\n");
     fflush(stdout);
 }
